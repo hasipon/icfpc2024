705b989cacbdc52b0f33d6c187db702fae0978a0
Shingo INADA <mecha.is06@gmail.com>
2024-06-29 05:41:35 +0900

    update lambdaman vis

--- visualizer/lambdaman/src/App.tsx
+++ visualizer/lambdaman/src/App.tsx
@@ -1,6 +1,9 @@
 import {memo, useEffect, useState} from 'react'
-import {Grid, Input, Slider, TextField} from "@mui/material";
+import {Grid, Input, MenuItem, Select, Slider, TextField} from "@mui/material";
+import axios from "axios";
 import './App.css'
+import {Simulate} from "react-dom/test-utils";
+import input = Simulate.input;
 
 const SvgContent = memo((props: {field: string, solution: string, time: number}) => {
     let t = props.time;
@@ -89,20 +92,105 @@ const SvgContent = memo((props: {field: string, solution: string, time: number})
 });
 
 function App() {
+    const [problemNo, setProblemNo] = useState<number>(1);
+    const [solutionList, setSolutionList] = useState<string[]>([]);
+    const [selectedSolution, setSelectedSolution] = useState<number>(0);
     const [inputText, setInputText] = useState<string>("###.#...\n" + "...L..##\n" + ".#######");
-    const [outputText, setOutputText] = useState<string>("LLL");
+    const [outputText, setOutputText] = useState<string>("");
     const [time, setTime] = useState<number>(3);
 
     useEffect(() => {
-        setTime(outputText.length);
+        setTime(Math.max(0, outputText.length - 1));
     }, [outputText])
+
+    useEffect(() => {
+        const update = async () => {
+            const resp = await axios.get<string>("http://34.146.140.6/repo/problems/lambdaman/" + problemNo + ".txt");
+            if (resp.status !== 200) {
+                return;
+            }
+
+            // const regex = /<a href="([^"]+)"/g;
+            setInputText(resp.data);
+        };
+
+        update();
+    }, [problemNo]);
+
+    useEffect(() => {
+        const update = async () => {
+            const resp = await axios.get<string>("http://34.146.140.6/repo/solutions/lambdaman/");
+            if (resp.status !== 200) {
+                return;
+            }
+
+            const regex = /<a href="([^"]+)"/g;
+            const matches = resp.data.matchAll(regex);
+            const list = ["None"];
+            for (const match of matches) {
+                if (match[1].startsWith("" + problemNo + "-")) {
+                    list.push(match[1]);
+                }
+            }
+
+            setSolutionList(list);
+        };
+
+        update();
+    }, [problemNo]);
+
+    useEffect(() => {
+        const update = async () => {
+            if (selectedSolution === 0) {
+                return;
+            }
+
+            if (solutionList.length <= selectedSolution) {
+                return;
+            }
+
+            const s = solutionList[selectedSolution];
+            const resp = await axios.get<string>("http://34.146.140.6/repo/solutions/lambdaman/" + s);
+            if (resp.status !== 200) {
+                return;
+            }
+
+            setOutputText(resp.data);
+        };
+
+        update();
+    }, [selectedSolution, solutionList]);
 
     return (
         <>
             <Grid container>
+                <Grid item m={0} xs={6}>
+                    <Input
+                        type="number"
+                        value={problemNo}
+                        onChange={(e) => {
+                            const v = Number(e.target.value);
+                            if (1 <= v && v <= 21) {
+                                setProblemNo(v);
+                            }
+                        }}
+                    />
+                </Grid>
+                <Grid item m={0} xs={6}>
+                    <Select
+                        sx={{display: "flex"}}
+                        value={selectedSolution}
+                        onChange={(e) => {
+                            const v = Number(e.target.value);
+                            setSelectedSolution(v);
+                        }}
+                    >
+                        {solutionList.map((s, index) => <MenuItem key={s} value={index}>{s}</MenuItem>)}
+                    </Select>
+                </Grid>
                 <Grid item xs={6}>
                     <TextField
-                        sx={{ display: "flex" }}
+                        sx={{display: "flex"}}
                         variant="outlined"
                         label="入力"
                         multiline
@@ -115,7 +203,7 @@ function App() {
                 </Grid>
                 <Grid item xs={6}>
                     <TextField
-                        sx={{ display: "flex" }}
+                        sx={{display: "flex"}}
                         variant="outlined"
                         label="出力"
                         multiline
@@ -129,7 +217,7 @@ function App() {
                 <Grid item m={1} xs={6}>
                     <Slider
                         value={time}
-                        max={outputText.length}
+                        max={outputText.length - 1}
                         onChange={(_e, newValue) => {
                             setTime(newValue as number);
                         }}
@@ -145,7 +233,7 @@ function App() {
                         inputProps={{
                             step: 1,
                             min: 0,
-                            max: outputText.length,
+                            max: outputText.length - 1,
                             type: 'number',
                             'aria-labelledby': 'input-slider',
                         }}
@@ -155,7 +243,7 @@ function App() {
                     <SvgContent
                         field={inputText}
                         solution={outputText}
-                        time={time} />
+                        time={time}/>
                 </Grid>
             </Grid>
         </>
