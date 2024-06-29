7b6173e149c672d67b9aa77825ccd13229dc6556
hasipon <hasipon@users.noreply.github.com>
2024-06-29 02:47:56 +0000

    score=79

--- hasi/a_lm6.py
+++ hasi/a_lm6.py
@@ -1,7 +1,7 @@
 import sys
 import requests
 tbl = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&\'()*+,-./:;<=>?@[\\]^_`|~ \n"
-msg = 'B. S3/,6%},!-"$!-!.[} B$ L" B$ v" B$ v" B$ v" SLLLLLLLLLLLLLLLLLLLLLLLLL L" B. v" v"'
+msg = 'B. S3/,6%},!-"$!-!.[} B$ L" B$ v" B$ v" SLLLLLLLL L" B. B. B. B. v" v" v" v" v"'
 print(msg)
 res = requests.post("https://boundvariable.space/communicate", headers={"Authorization":"Bearer 92af6ee1-e632-462c-8baa-0d26799620d6"}, data=msg)
 assert res.ok
