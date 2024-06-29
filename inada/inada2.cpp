0d16d84efab17c87fc624601526c89ab40290c61
Shingo INADA <mecha.is06@gmail.com>
2024-06-29 20:24:06 +0900

    update solution

--- inada/inada2.cpp
+++ inada/inada2.cpp
@@ -16,14 +16,65 @@ public:
     int W{}, H{};
     vector<string> input;
 
-    string solve() {
+    bool inside(int x, int y) const {
+        return 0 <= x && x < W && 0 <= y && y < H;
+    }
+
+    string solve2() {
         auto [lx, ly, dot_count] = preprocess(input);
         vector got(H, vector<bool>(W));
         string ans;
+
+        while (dot_count) {
+            int best_r = 0;
+            int best_len = 1 << 30;
+            for (int r = 0; r < 4; r++) {
+                const int nx = lx + dx[r];
+                const int ny = ly + dy[r];
+                if (inside(nx, ny) && input[ny][nx] != '#') {
+                    const int len = solve(ans + dirs[r]).size();
+                    if (len < best_len) {
+                        best_r = r;
+                        best_len = len;
+                    }
+                }
+            }
+
+            ans += dirs[best_r];
+            lx += dx[best_r];
+            ly += dy[best_r];
+            if (input[ly][lx] == '.' && !got[ly][lx]) {
+                got[ly][lx] = true;
+                dot_count--;
+            }
+            cerr << "lambda (x, y) = " << lx << " " << ly << " " << " dot=" << dot_count << " " << ans << endl;
+        }
+
+        return ans;
+    }
+
+    string solve(string half_ans) {
+        auto [lx, ly, dot_count] = preprocess(input);
+        vector got(H, vector<bool>(W));
+
+        int last_r = 0;
+        for (int turn = 0; turn < half_ans.size(); turn++) {
+            int r = dirs.find(half_ans[turn]);
+            lx += dx[r];
+            ly += dy[r];
+
+            if (input[ly][lx] == '.' && !got[ly][lx]) {
+                dot_count--;
+                got[ly][lx] = true;
+            }
+            last_r = r;
+        }
+
+        string ans = half_ans;
         for (int t = 0; t < dot_count; t++) {
             vector visit(H, vector<int>(W));
             queue<tuple<int, int, string> > Q;
-            cerr << "lambda (x, y) = " << lx << " " << ly << " " << ans << endl;
+            // cerr << "lambda (x, y) = " << lx << " " << ly << " " << ans << endl;
             Q.emplace(lx, ly, "");
 
             while (!Q.empty()) {
@@ -34,7 +85,7 @@ public:
                 for (int r = 0; r < 4; r++) {
                     const int nx = px + dx[r];
                     const int ny = py + dy[r];
-                    if (0 <= nx && nx < W && 0 <= ny && ny < H) {
+                    if (inside(nx, ny)) {
                         if (input[ny][nx] == '.' && !got[ny][nx]) {
                             lx = nx;
                             ly = ny;
@@ -57,10 +108,6 @@ public:
             }
         }
 
-        return ans;
-    }
-
-    string improve(const string& ans) {
         return ans;
     }
 
@@ -95,7 +142,6 @@ vector<string> read_input(const string& path) {
     return input;
 }
 
-
 int main(int argc, char *argv[]) {
     if (argc <= 1) {
         cerr << "need problem file path" << endl;
@@ -104,8 +150,7 @@ int main(int argc, char *argv[]) {
 
     Solver solver;
     solver.input = read_input(argv[1]);
-    auto ans = solver.solve();
-    ans = solver.improve(ans);
+    auto ans = solver.solve2();
 
     cerr << ans.size() << endl;
     cout << ans << endl;
