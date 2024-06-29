6f012d508c60c88ef8e107da89ef00537cffbcc4
Shingo INADA <mecha.is06@gmail.com>
2024-06-29 16:01:27 +0900

    vis fixes

--- visualizer/spaceship/src/App.tsx
+++ visualizer/spaceship/src/App.tsx
@@ -3,15 +3,21 @@ import {Grid, Input, MenuItem, Select, Slider, TextField} from "@mui/material";
 import axios from "axios";
 import './App.css'
 
-const gcd = (x:number, y:number) => x % y ? gcd(y, x % y) : y;
-
 const SvgContent = memo((props: {input: string, solution: string, time: number}) => {
     let t = props.time;
     if (t < 0) {
         t = 0;
     }
 
+    if (!props.input) {
+        return;
+    }
+
     const stage = props.input.trim().split(/\r?\n/).map(line => line.split(" ").map(v => Number(v)));
+    if (!stage) {
+        return;
+    }
+
     const targets = new Set();
     let min_x = -10, max_x = -10, min_y = 10, max_y = 10;
     for (let i = 0; i < stage.length; i++) {
@@ -27,6 +33,7 @@ const SvgContent = memo((props: {input: string, solution: string, time: number})
     const W = max_x - min_x;
     const H = max_y - min_y;
 
+    console.log(min_x, min_y, max_x, max_y);
     console.log(W, H);
     console.log(stage);
 
@@ -71,6 +78,7 @@ const SvgContent = memo((props: {input: string, solution: string, time: number})
                 targets.delete(`${xx}_${yy}`);
             }
         } else if (vx !== 0 && vy !== 0) {
+            const gcd = (x:number, y:number) => x % y ? gcd(y, x % y) : y;
             const num_p = gcd(Math.abs(vx), Math.abs(vy));
             for (let dt = 0; dt < num_p; dt++) {
                 const xx = px - vx * (dt + 1) / num_p;
@@ -92,11 +100,16 @@ const SvgContent = memo((props: {input: string, solution: string, time: number})
         const x = stage[i][0];
         const y = stage[i][1];
         if (targets.has(`${x}_${y}`)) {
-            svgChildren.push( <rect key={"rect_" + i} x={x} y={flip_y(y)} width={r} height={r} fill={"#444444"}></rect> );
+            svgChildren.push(
+                <rect key={"rect_" + i} x={x} y={flip_y(y)} width={r} height={r} fill={"#444444"}></rect>
+            );
         }
     }
 
-    svgChildren.push( <rect key={"rect_p"} x={px} y={flip_y(py)} width={r} height={r} fill={"#CC0000"}></rect> );
+    console.log(px);
+    svgChildren.push(
+        <rect key={"rect_p"} x={px} y={flip_y(py)} width={r} height={r} fill={"#CC0000"}></rect>
+    );
 
     return <svg width="1024" height="1024" viewBox={"" + (min_x) + " " + (min_y) + " " + (W+r) + " " + (H+r)} id="game">{svgChildren}</svg>
 });
@@ -105,7 +118,7 @@ function App() {
     const [problemNo, setProblemNo] = useState<number>(1);
     const [solutionList, setSolutionList] = useState<string[]>([]);
     const [selectedSolution, setSelectedSolution] = useState<number>(0);
-    const [inputText, setInputText] = useState<string>("###.#...\n" + "...L..##\n" + ".#######");
+    const [inputText, setInputText] = useState<string>("");
     const [outputText, setOutputText] = useState<string>("");
     const [time, setTime] = useState<number>(3);
 
