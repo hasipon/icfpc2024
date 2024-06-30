8767074ed9e33811394ef67410e4d8f649de2ef0
shohei909 <909mmsphere@gmail.com>
2024-06-30 22:37:12 +0900

    solve by hand

--- solutions/lambdaman3.py
+++ solutions/lambdaman3.py
@@ -74,7 +74,6 @@ for i in range(10, 11):
 	
     res = requests.post("https://boundvariable.space/communicate", headers={"Authorization":"Bearer 92af6ee1-e632-462c-8baa-0d26799620d6"}, data=msg)
     assert res.ok
-    result = ''.join(tbl[ord(x)-33] for x in res.text[1:])
     print("\n" + res.text)
     result = ''.join(tbl[ord(x)-33] for x in res.text[1:])
     print(result)
